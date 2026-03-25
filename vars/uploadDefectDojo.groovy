92db2aa1bf8977c34d5fafc3fd63f2edec6e7541
chengdevith <chengdevith5@gmail.com>
2026-03-25 03:27:17 +0000

    jenkins: fix trivyScan file

--- vars/uploadDefectDojo.groovy
+++ vars/uploadDefectDojo.groovy
@@ -1,16 +1,26 @@
 def call(Map config = [:]) {
-    sh '''
-        curl --fail -k -X POST "${DEFECTDOJO_URL}/api/v2/reimport-scan/" \
-          -H "Authorization: Token ${DEFECTDOJO_API_KEY}" \
-          -F "scan_type=Trivy Scan" \
-          -F "file=@${REPORT_PATH}" \
-          -F "auto_create_context=true" \
-          -F "product_type_name=${PRODUCT_TYPE_NAME}" \
-          -F "product_name=${PRODUCT_NAME}" \
-          -F "engagement_name=${ENGAGEMENT_NAME}" \
-          -F "test_title=${TEST_TITLE}" \
-          -F "active=true" \
-          -F "verified=true" \
-          -F "close_old_findings=true"
-    '''
+    def defectdojoUrl        = config.defectdojoUrl        ?: error('uploadDefectDojo: defectdojoUrl is required')
+    def defectdojoCredentialId = config.defectdojoCredentialId ?: error('uploadDefectDojo: defectdojoCredentialId is required')
+    def reportPath           = config.reportPath           ?: '/home/enz/trivy/reports/trivy-report.json'
+    def productTypeName      = config.productTypeName      ?: 'Web Applications'
+    def productName          = config.productName          ?: error('uploadDefectDojo: productName is required')
+    def engagementName       = config.engagementName       ?: 'Jenkins'
+    def testTitle            = config.testTitle            ?: 'Trivy Image Scan'
+
+    withCredentials([string(credentialsId: defectdojoCredentialId, variable: 'DEFECTDOJO_API_KEY')]) {
+        sh """
+            curl --fail -k -X POST "${defectdojoUrl}/api/v2/reimport-scan/" \
+              -H "Authorization: Token \${DEFECTDOJO_API_KEY}" \
+              -F "scan_type=Trivy Scan" \
+              -F "file=@${reportPath}" \
+              -F "auto_create_context=true" \
+              -F "product_type_name=${productTypeName}" \
+              -F "product_name=${productName}" \
+              -F "engagement_name=${engagementName}" \
+              -F "test_title=${testTitle}" \
+              -F "active=true" \
+              -F "verified=true" \
+              -F "close_old_findings=true"
+        """
+    }
 }
