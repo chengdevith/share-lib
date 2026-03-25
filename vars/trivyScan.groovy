3e344c7ab9a0188dec477eb14840a5842cf689e5
chengdevith <chengdevith5@gmail.com>
2026-03-25 03:23:29 +0000

    jenkins: fix trivyScan file

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,11 +1,20 @@
 def call(Map config = [:]) {
-    sh '''
-        mkdir -p /home/enz/trivy/reports
-        docker compose -f ${TRIVY_PATH} exec -T trivy \
-        trivy image \
-        --scanners vuln,secret,misconfig \
-        --format json \
-        -o /reports/trivy-report.json \
-        ${FULL_IMAGE}
-    '''
+    def fullImage   = config.fullImage   ?: error('trivyScan: fullImage is required')
+    def trivyPath   = config.trivyPath   ?: '/home/enz/trivy/docker-compose.yml'
+    def reportPath  = config.reportPath  ?: '/home/enz/trivy/reports/trivy-report.json'
+    def gateSeverity = config.gateSeverity ?: 'HIGH,CRITICAL'
+
+    def reportDir = reportPath.substring(0, reportPath.lastIndexOf('/'))
+
+    sh """
+        mkdir -p ${reportDir}
+        docker compose -f ${trivyPath} exec --no-TTY trivy \
+            trivy image \
+            --scanners vuln,secret,misconfig \
+            --exit-code 1 \
+            --severity ${gateSeverity} \
+            --format json \
+            -o /reports/trivy-report.json \
+            ${fullImage}
+    """
 }
