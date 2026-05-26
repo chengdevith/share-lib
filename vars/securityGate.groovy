7385cbfb8f5faa3f64b6e5de6209ef3b0671420e
Vuthy Tourn <vuthytuon168@gmail.com>
2026-05-26 08:13:47 +0700

    Fix variable assignment and update docker command

--- vars/securityGate.groovy
+++ vars/securityGate.groovy
@@ -1,12 +1,14 @@
 def call(Map config = [:]) {
-    def severity = config.severity ?: 'HIGH,CRITICAL'
+    def severity  = config.severity ?: 'HIGH,CRITICAL'
+    def trivyPath = config.trivyPath ?: env.TRIVY_PATH ?: error('securityGate: trivyPath is required')
+    def fullImage = config.fullImage ?: env.FULL_IMAGE ?: error('securityGate: fullImage is required')
 
     sh """
-        docker compose -f ${TRIVY_PATH} exec -T trivy \
+        docker compose -f ${trivyPath} exec -T trivy \
           trivy image \
           --severity ${severity} \
           --exit-code 1 \
           --no-progress \
-          ${FULL_IMAGE}
+          ${fullImage}
     """
 }
